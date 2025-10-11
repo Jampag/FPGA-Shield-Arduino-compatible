65e9de20d20465513528b4d702eb190e3dc2f71c
Jampag <114861733+Jampag@users.noreply.github.com>
2025-10-11 17:33:04 +0200

    Create AND_gate.vhd

---
+++ example/AND_gate/AND_gate.vhd
@@ -0,0 +1,38 @@
+----------------------------------------------------------------------------------
+-- Autor: Jampag
+-- 
+-- Create Date: 10.10.2025 
+-- Design Name: AND_gate`
+-- Module Name: AND_gate
+-- Project Name: AND_gate
+-- Target Devices: MODULO FPGA Ver3
+-- Tool Versions: 2024.2
+-- Description:  
+--      A simple AND gate project
+-- Dependencies: 
+-- Additional Comments:x
+-- 
+----------------------------------------------------------------------------------
+-- Library 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+
+-- Entity Declaration
+entity AND_gate is
+    port (
+        A		: in std_logic; 
+        B		: in std_logic;
+        Y		: out std_logic);
+end AND_gate;
+
+-- Architecture
+architecture Behavioral of AND_gate is
+
+-- Begin Architecture
+begin
+
+    -- Signal Assignments
+    Y <= (A AND B);
+    
+end Behavioral;
