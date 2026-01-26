406c4804f7ab324d3be1bf1f9a486fe5946eae59
Jampag <114861733+Jampag@users.noreply.github.com>
2026-01-26 21:26:39 +0100

    Add LED toggle functionality in VHDL
    This VHDL file implements a toggle mechanism for an LED based on a button press, changing the LED status when the button is released.

---
+++ example/debounce/LED_toggle.vhd
@@ -0,0 +1,46 @@
+-------------------------------------------------------------------------------
+-- File : LED_toggle
+-- Autor : Jampag
+-- Data : 2026 Jan 25
+-- Description: Change led status when btn is relesad
+--                __    __    __    __  
+--     clk       ^  |  ^  |  ^  |  ^  | 
+--             __|  |__|  |__|  |__|  |_
+--             ____            ___________
+--     SW1         |          |     
+--      (D)        |__________|
+--             ________             ___________
+--     SW1_reg         |           |     
+--      (Q)            |___________|
+--                  ^^^^
+--                    |_toggle led    
+--Note :.
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+-- Entity 
+entity LED_toggle is
+    Port (
+        L1    : out std_logic;
+        clk   : in std_logic;
+        SW1   : in std_logic
+        );
+end LED_toggle; 
+
+-- Architecture
+architecture behavior of LED_toggle is
+    signal L1_reg    :std_logic := '0'; 
+    signal SW1_reg    :std_logic := '0'; 
+  begin
+    process(Clk)
+    begin 
+        if rising_edge(Clk) then
+            SW1_reg <= SW1;
+            if SW1 = '0' and SW1_reg = '1' then
+                L1_reg <= not L1_reg;
+            end if;
+        end if;
+    end process;
+  L1 <= L1_reg;  
+end behavior;
+
